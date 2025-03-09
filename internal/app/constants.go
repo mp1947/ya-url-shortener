de4d47c183e525779f0c9dd5f6a636bf84aa9aba
Maksim Poliakov <m.poliakov1337@gmail.com>
2025-03-09 20:13:03 +0300

    remove internal-app

--- internal/app/constants.go
+++
@@ -1,8 +0,0 @@
-package app
-
-// internal constants
-const (
-	charSet              = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	randomIDStringLength = 8
-	contentType          = "text/plain; charset=utf-8"
-)
--- internal/app/types.go
+++
@@ -1,7 +0,0 @@
-package app
-
-// Urls represents a mapping between unique identifiers
-// and their corresponding original not-shortened URLs.
-type Urls struct {
-	IDToURL map[string]string
-}
